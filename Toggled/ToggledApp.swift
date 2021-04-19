7fe35a153630c21d0d6dbcdd238cf8184ea002f3
Bug Williams <zachwilliams.services@gmail.com>
2021-04-19 15:00:16 -0400

    Initial Commit

---
+++ Toggled/ToggledApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggledApp.swift
+//  Toggled
+//
+//  Created by Bug on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ToggledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
